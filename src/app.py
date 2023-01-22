59d7150730c6d5b80a73654a194f8818158251da
Sahil Pattni <sahilpattni97@gmail.com>
2023-01-22 03:55:03 -0500

    Re-formatted

--- src/app.py
+++ src/app.py
@@ -32,41 +32,67 @@ no_refresh = 0
 last_row = None
 while True:
     start_time = time.time()
-    df = streamer.stream()
+    df = streamer.box_stream()
     with placeholder.container():
         fig_col1, fig_col2 = st.columns(2)
 
-        with fig_col1:
-            refresh_time_padded = 1 if last_refresh_time < 1 else last_refresh_time
-            st.markdown(f"### Live Orders: {df.RoundedTimeStamp.iloc[0]} to {df.RoundedTimeStamp.iloc[-1]}")
+        refresh_time_padded = 1 if last_refresh_time < 1 else last_refresh_time
+        st.markdown(f"### Live Orders: {df.RoundedTimeStamp.iloc[0]} to {df.RoundedTimeStamp.iloc[-1]}")
 
-            fig = px.scatter(
-                df[df.OrderPrice.notna()],
-                x="TimeStamp",
-                y="OrderPrice",
-                color="Symbol",
-                color_discrete_map=streamer.get_color_map(),
-                title=f"Refresh Time: {refresh_time_padded:.3f} second(s) | Open Orders: {streamer.get_nopen_orders():,}",
-                labels={"OrderPrice": "Order Price", "TimeStamp": "Time"})
-            st.write(fig)
+        fig = px.scatter(
+            df[df.MessageType.isin(['Trade', 'NewOrderAcknowledged'])],
+            x="TimeStamp",
+            y="OrderPrice",
+            color="Symbol",
+            color_discrete_map=streamer.get_ticker_color_map(),
+            title=f"Refresh Time: {refresh_time_padded:.3f} second(s) | Open Orders: {streamer.get_nopen_orders():,}",
+            labels={"OrderPrice": "Order Price", "TimeStamp": "Time"})
+        # Set category order
+        fig.update_xaxes(categoryorder='category ascending')
+        st.plotly_chart(fig)
+        
 
-        with fig_col2:
-            cancelled_orders = streamer.get_cancelled_orders()
-            executed_trades = streamer.get_executed_trades()
-            open_orders = streamer.get_nopen_orders()
+        st.markdown(f"### Anomalies Detected (Incorrect Flow)")
+        st.write(streamer.get_anomalies())
 
-            equitymeans = df.groupby('Symbol').mean()
-            fig = px.bar(
-                equitymeans,
-                x=equitymeans.index,
-                y='OrderPrice',
-                color=equitymeans.index,
-                title='Avg Price per second'
-            )
-            st.write(fig)
+        # Plot percentage of cancelled orders and executed trades
+        fig = px.pie(
+            values=[streamer.get_cancelled_orders(), streamer.get_executed_trades()],
+            names=['Cancelled Orders', 'Executed Trades'],
+            title=f"Cancelled Orders: {streamer.get_cancelled_orders():,} | Executed Trades: {streamer.get_executed_trades():,}"
+        )
+        st.plotly_chart(fig)
+
+
+        # # with fig_col2:
+        # fig = px.treemap(
+        #     df,
+        #     path=['MessageType'],
+        #     values=pd.Series(np.ones(df.shape[0])),
+        #     color='MessageType',
+        #     color_discrete_map=streamer.get_message_color_map(),
+        # )
+        # st.plotly_chart(fig)
+
+
+        # cancelled_orders = streamer.get_cancelled_orders()
+        # executed_trades = streamer.get_executed_trades()
+        # open_orders = streamer.get_nopen_orders()
+
+        # equitymeans = df.groupby('Symbol').mean()
+        # fig = px.bar(
+        #     equitymeans,
+        #     x=equitymeans.index,
+        #     y='OrderPrice',
+        #     color=equitymeans.index,
+        #     title='$x^2$'
+        # )
+        # fig.update_xaxes(categoryorder='category ascending')
+        # st.plotly_chart(fig)
             
         st.header(f"Order Book: {df.shape[0]:,} row(s) from {df.index[0]:,} to {df.index[-1]:,}")
-        
+        st.dataframe(df.tail(10))
+
         # Sleep for remaining time (up to 1 second)
         elapsed_time = time.time() - start_time
         if elapsed_time < 1:
@@ -82,5 +108,3 @@ while True:
             if no_refresh > 3:
                 break
         last_row = df.index[-1]
-        
-        st.dataframe(df.tail(10))
